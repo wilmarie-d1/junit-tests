0eee2553f95c365a2016ee0c22e2e0e8874e79ac
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-17 11:23:25 -0600

    getter and setter added

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -10,4 +10,22 @@ public class Student {
         this.name = name;
         this.grades = new ArrayList<>();
     }
+
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void addGrade(int grade){
+
+    }
+
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+    public  double getGradeAverage(){
+
+    }
+
 }
